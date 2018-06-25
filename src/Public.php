f2718c238e967215e55c14c36b7ce8ab556ad53c
pxgamer <owzie123@gmail.com>
2018-06-25 14:57:02 +0100

    Add getOrderTicker() method

--- src/Public.php
+++ src/Public.php
@@ -30,6 +30,24 @@ class Basic
     }
 
     /**
+     * Retrieve the list of orders for a specific market from start time to end time.
+     *
+     * @param string   $marketName
+     * @param int|null $startTime
+     * @param int|null $endTime
+     * @return mixed
+     */
+    public function getOrderTicker(string $marketName, int $startTime = null, int $endTime = null)
+    {
+        return $this->call('public/getmarketsummaries?'.
+            http_build_query([
+                'market' => $marketName,
+                'start'  => $startTime,
+                'end'    => $endTime,
+            ]));
+    }
+
+    /**
      * Retrieve the list of market summaries.
      *
      * @return mixed
