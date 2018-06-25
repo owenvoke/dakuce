2709a45e15fdfce333e19c5ad634a99959ced328
pxgamer <owzie123@gmail.com>
2018-06-25 14:28:23 +0100

    Add initial Dakuce class

---
+++ src/Dakuce.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace pxgamer\Dakuce;
+
+/**
+ * Class Dakuce
+ */
+class Dakuce
+{
+    /**
+     * The base URI for the Dakuce main site.
+     */
+    public const BASE_URI = 'https://dakuce.com';
+    /**
+     * The base URI for the Dakuce API.
+     */
+    public const API_BASE_URI = 'https://dakuce.com/api/v1.0/';
+}
